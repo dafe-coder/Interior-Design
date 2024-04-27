fe3e356951fd39ab7f9fbbfe4caab7a669988797
Dmitriy <aua008@gmail.com>
2024-04-27 23:18:27 +0300

    Project settings, added colors, variables, font-family, plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,32 @@
 /** @type {import('tailwindcss').Config} */
-
 const colors = require('tailwindcss/colors')
-
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
 	future: {},
 	theme: {
-		extend: {},
+		container: {
+			center: true,
+		},
+		extend: {
+			backgroundImage: {
+				kitchen: "url('./src/images/kitchen.png')",
+			},
+		},
 		colors: {
-			...colors,
+			white: colors.white,
 			green: {
+				...colors.green,
 				200: '#B1FFBB',
 				400: 'rgba(177, 255, 187, 0.16)',
 				500: '#26413A',
 				600: '#0B1D22',
 			},
 			brown: {
+				...colors.brown,
 				500: '#131313',
 			},
 			gray: {
+				...colors.gray,
 				300: '#959595',
 				500: '#4A4A4A',
 			},
@@ -25,6 +34,9 @@ module.exports = {
 				400: '#FDD0A1',
 			},
 		},
+	},
+	fontFamily: {
+		roboto: ['Roboto', ...defaultTheme.fontFamily.sans],
 	},
 	content: ['./**/*.{html,js}'],
 	variants: {},
